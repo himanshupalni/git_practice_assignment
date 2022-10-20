4be5b8729bc86427156f07be83cf6ab6fb3b7ace
himanshu <himanshupalni61@gmail.com>
2022-10-20 09:12:16 +0530

    added palindrome file

---
+++ palindrome.js
@@ -0,0 +1,8 @@
+ for (let i = num-1; i<num;i--) 
+           if(num !== i){
+               console.log("Yes");
+               
+           } else {
+               console.log("No");
+           }
+}
